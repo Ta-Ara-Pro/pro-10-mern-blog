5b578e7db4cec87e471bb898eb84738df6992745
heren.pro <167914557+Ta-Ara-Pro@users.noreply.github.com>
2024-12-27 11:31:43 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 
 
 const corsOptions = {
-    origin: 'https://mern-blog-front-p10.vercel.app/', 
+    origin: 'https://mern-blog-front-p10.vercel.app', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow cookies to be sent
 };
